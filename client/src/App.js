bf3069ccf6bc3c473f37d6f89083bebb8c619084
nettlesoup <sanettleingham@hotmail.co.uk>
2021-08-04 14:38:35 +0100

    Create new react app with PostCreate form

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { PostCreate } from './PostCreate';
+
+export const App = () => {
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
