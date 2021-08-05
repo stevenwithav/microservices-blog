f47e13785dab4c4b397937212de5cb4b33d78f29
nettlesoup <sanettleingham@hotmail.co.uk>
2021-08-05 15:19:54 +0100

    Create basic event bus and query service

--- comments/index.js
+++ comments/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -13,20 +18,35 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({
-    id: commentId, content
+    id: commentId,
+    content,
   });
 
   commentsByPostId[req.params.id] = comments;
 
-  res.status(201).send(comments);
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
+  res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
